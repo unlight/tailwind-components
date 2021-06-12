ed9513bb8b740f4a98ba47e47e4e6850a57ee590
Roman_Vasilev <s91630277@gmail.com>
2021-06-12 23:13:51 +0400

    Fix daisyui

--- src/scrapers/daisyui.ts
+++ src/scrapers/daisyui.ts
@@ -3,25 +3,27 @@ import { CompomentLink, ScraperArgs } from '../types';
 
 export default async function ({ page }: ScraperArgs): Promise<CompomentLink[]> {
     const result: CompomentLink[] = [];
-    await page.goto('https://daisyui.netlify.app/', { waitUntil: 'networkidle0' });
-    const links = await page.$$eval('li > a[href^="/components/"]', (elements) => {
-        return elements.map((a) => ({
+    await page.goto('https://daisyui.com/components/button', {
+        waitUntil: 'networkidle0',
+    });
+    const links = await page.$$eval('li > a[href^="/components/"]', elements => {
+        return elements.map(a => ({
             link: (a as HTMLAnchorElement).href,
             category: a.textContent!.trim(),
         }));
     });
     for (const { link, category } of links) {
         await page.goto(link);
-        let names = await page.$$eval('main .text-xs.capitalize', (elements) => {
-            return elements.map((element) => element.textContent!.trim());
+        let names = await page.$$eval('main .text-xs.capitalize', elements => {
+            return elements.map(element => element.textContent!.trim());
         });
         names = uniq(names);
-        names.forEach((name) => {
+        for (let name of names) {
             if (!name.startsWith(category)) {
                 name = `${category} ${name}`;
             }
             result.push({ link, name });
-        });
+        }
     }
 
     return result;
