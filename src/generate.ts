ac9289dfa524c39d12f3a5f103fa9259e2bee6cd
Roman_Vasilev <s91630277@gmail.com>
2022-05-10 01:50:25 +0400

    style: Format

--- src/generate.ts
+++ src/generate.ts
@@ -5,342 +5,339 @@ import _ from 'lodash';
 import fs from 'fs/promises';
 
 type GenerateArgs = {
-    items: CompomentLink[];
+  items: CompomentLink[];
 };
 
 const categoryList = [
-    new Category({
-        name: 'Accordion',
-        keywords: [
-            new Keyword('accordion'),
-            new Keyword('collapsible'),
-            new Keyword('collapse'),
-        ],
-    }),
-    new Category({
-        name: 'Alert/Notification',
-        keywords: [
-            new Keyword('Alert'),
-            new Keyword('notification'),
-            new Keyword('toast'),
-            new Keyword('snackbar'),
-            new Keyword('Callout'),
-        ],
-    }),
-    new Category({ name: 'Avatar' }),
-    new Category({ name: 'Badge' }),
-    new Category({ name: 'Breadcrumb' }),
-    new Category({ name: 'Button' }),
-    new Category({
-        name: 'Call to Action',
-        keywords: [new Keyword('call to action', 2)],
-    }),
-    new Category({ name: 'Card' }),
-    new Category({
-        name: 'Dashboard Widgets',
-        keywords: [
-            new Keyword('dashboard widgets', 2),
-            new Keyword('statistic cards', 2),
-            new Keyword('statistic widgets', 2),
-            new Keyword('statistic'),
-            new Keyword('weather ui component', 5),
-        ],
-    }),
-    new Category({
-        name: 'Slider/Carousel',
-        keywords: [new Keyword('carousel'), new Keyword('slider')],
-    }),
-    new Category({
-        name: 'Cookies',
-        keywords: [new Keyword('Cookie Policy'), new Keyword('cookie')],
-    }),
-    new Category({
-        name: 'Date/Time',
-        keywords: [
-            new Keyword('calendar'),
-            new Keyword('datepicker'),
-            new Keyword('date picker'),
-            new Keyword('date picker'),
-            new Keyword('timepicker'),
-            new Keyword('time picker'),
-        ],
-    }),
-    new Category({
-        name: 'Dropdown',
-        keywords: [
-            new Keyword('flyout'),
-            new Keyword('fly-out'),
-            new Keyword('multi select'),
-            new Keyword('drop down'),
-            new Keyword('select'),
-            new Keyword('popover'),
-        ],
-    }),
-    new Category({
-        name: 'Form',
-        keywords: [
-            new Keyword('input', 1),
-            new Keyword('textfield', 2),
-            new Keyword('text field', 2),
-            new Keyword('textarea', 1),
-            new Keyword('checkbox', 1),
-            new Keyword('radio', 1),
-        ],
-    }),
-    new Category({
-        name: 'Contact',
-        parent: 'Form',
-        keywords: [new Keyword('contact form', 10)],
-    }),
-    new Category({
-        name: 'Login',
-        parent: 'Form',
-        keywords: [
-            new Keyword('login form', 10),
-            new Keyword('sign-in', 5),
-            new Keyword('sign in', 5),
-            new Keyword('form login', 5),
-            new Keyword('login with', 3),
-        ],
-    }),
-    new Category({
-        name: 'Register',
-        parent: 'Form',
-        keywords: [
-            new Keyword('registration', 2),
-            new Keyword('sign-up', 2),
-            new Keyword('sign up', 2),
-            new Keyword('signup', 2),
-            new Keyword('auth components', 2),
-        ],
-    }),
-    new Category({
-        name: 'Search',
-        parent: 'Form',
-        keywords: [new Keyword('search box', 5)],
-    }),
+  new Category({
+    name: 'Accordion',
+    keywords: [
+      new Keyword('accordion'),
+      new Keyword('collapsible'),
+      new Keyword('collapse'),
+    ],
+  }),
+  new Category({
+    name: 'Alert/Notification',
+    keywords: [
+      new Keyword('Alert'),
+      new Keyword('notification'),
+      new Keyword('toast'),
+      new Keyword('snackbar'),
+      new Keyword('Callout'),
+    ],
+  }),
+  new Category({ name: 'Avatar' }),
+  new Category({ name: 'Badge' }),
+  new Category({ name: 'Breadcrumb' }),
+  new Category({ name: 'Button' }),
+  new Category({
+    name: 'Call to Action',
+    keywords: [new Keyword('call to action', 2)],
+  }),
+  new Category({ name: 'Card' }),
+  new Category({
+    name: 'Dashboard Widgets',
+    keywords: [
+      new Keyword('dashboard widgets', 2),
+      new Keyword('statistic cards', 2),
+      new Keyword('statistic widgets', 2),
+      new Keyword('statistic'),
+      new Keyword('weather ui component', 5),
+    ],
+  }),
+  new Category({
+    name: 'Slider/Carousel',
+    keywords: [new Keyword('carousel'), new Keyword('slider')],
+  }),
+  new Category({
+    name: 'Cookies',
+    keywords: [new Keyword('Cookie Policy'), new Keyword('cookie')],
+  }),
+  new Category({
+    name: 'Date/Time',
+    keywords: [
+      new Keyword('calendar'),
+      new Keyword('datepicker'),
+      new Keyword('date picker'),
+      new Keyword('date picker'),
+      new Keyword('timepicker'),
+      new Keyword('time picker'),
+    ],
+  }),
+  new Category({
+    name: 'Dropdown',
+    keywords: [
+      new Keyword('flyout'),
+      new Keyword('fly-out'),
+      new Keyword('multi select'),
+      new Keyword('drop down'),
+      new Keyword('select'),
+      new Keyword('popover'),
+    ],
+  }),
+  new Category({
+    name: 'Form',
+    keywords: [
+      new Keyword('input', 1),
+      new Keyword('textfield', 2),
+      new Keyword('text field', 2),
+      new Keyword('textarea', 1),
+      new Keyword('checkbox', 1),
+      new Keyword('radio', 1),
+    ],
+  }),
+  new Category({
+    name: 'Contact',
+    parent: 'Form',
+    keywords: [new Keyword('contact form', 10)],
+  }),
+  new Category({
+    name: 'Login',
+    parent: 'Form',
+    keywords: [
+      new Keyword('login form', 10),
+      new Keyword('sign-in', 5),
+      new Keyword('sign in', 5),
+      new Keyword('form login', 5),
+      new Keyword('login with', 3),
+    ],
+  }),
+  new Category({
+    name: 'Register',
+    parent: 'Form',
+    keywords: [
+      new Keyword('registration', 2),
+      new Keyword('sign-up', 2),
+      new Keyword('sign up', 2),
+      new Keyword('signup', 2),
+      new Keyword('auth components', 2),
+    ],
+  }),
+  new Category({
+    name: 'Search',
+    parent: 'Form',
+    keywords: [new Keyword('search box', 5)],
+  }),
 
-    new Category({
-        name: 'Upload',
-        parent: 'Form',
-        keywords: [new Keyword('file upload', 8), new Keyword('file drop', 2)],
-    }),
-    new Category({ name: 'Footer', keywords: [new Keyword('footer', 2)] }),
-    new Category({ name: 'Hero' }),
-    new Category({
-        name: 'Loading/Spinner',
-        keywords: [
-            new Keyword('spinner'),
-            new Keyword('loading'),
-            new Keyword('loader dot'),
-        ],
-    }),
-    new Category({
-        name: 'Modal',
-        keywords: [
-            new Keyword('modal'),
-            new Keyword('popup'),
-            new Keyword('popup box', 2),
-        ],
-    }),
-    new Category({
-        name: 'Navigation/Header',
-        keywords: [
-            new Keyword('navigation'),
-            new Keyword('header'),
-            new Keyword('navbar', 2),
-            new Keyword('nav menu'),
-            new Keyword('navabr'),
-            new Keyword('navbars with', 5),
-            new Keyword('responsive navbar', 8),
-            new Keyword('navigation bar', 10),
-            new Keyword('navs'),
-        ],
-    }),
-    new Category({ name: 'Page' }),
-    new Category({
-        name: '404',
-        parent: 'Page',
-        keywords: [new Keyword('404'), new Keyword('not found', 2)],
-    }),
-    new Category({
-        name: 'Pricing',
-        parent: 'Page',
-        keywords: [new Keyword('pricing', 2)],
-    }),
-    new Category({
-        name: 'Testimonial',
-        parent: 'Page',
-        keywords: [new Keyword('testimonial', 2), new Keyword('testimony', 2)],
-    }),
-    new Category({ name: 'Pagination' }),
-    new Category({ name: 'Progress Bar', keywords: [new Keyword('progress')] }),
-    new Category({
-        name: 'Skeleton',
-        keywords: [new Keyword('skeleton')],
-    }),
-    new Category({
-        name: 'Sidebar',
-        keywords: [
-            new Keyword('side panel', 2),
-            new Keyword('side nav', 2),
-            new Keyword('slide-overs', 2),
-            new Keyword('drawer', 2),
-            new Keyword('sliding', 2),
-            new Keyword('Sidenav', 2),
-        ],
-    }),
-    new Category({ name: 'Comment', keywords: [new Keyword('comment form', 2)] }),
-    new Category({ name: 'Step', keywords: [new Keyword('multistep')] }),
-    new Category({
-        name: 'Switch',
-        keywords: [
-            new Keyword('toggle'),
-            new Keyword('toogle'),
-            new Keyword('toggle button', 2),
-        ],
-    }),
-    new Category({
-        name: 'Tags',
-        keywords: [
-            new Keyword('tagline'),
-            new Keyword('pills'),
-            new Keyword('tag line'),
-            new Keyword('chips'),
-            new Keyword('label-tag'),
-        ],
-    }),
-    new Category({
-        name: 'Tooltips',
-        keywords: [new Keyword('tooltip')],
-    }),
-    new Category({ name: 'Table' }),
-    new Category({ name: 'List' }),
-    new Category({ name: 'Tab' }),
-    new Category({ name: 'Timeline' }),
-    new Category({ name: 'Other' }),
+  new Category({
+    name: 'Upload',
+    parent: 'Form',
+    keywords: [new Keyword('file upload', 8), new Keyword('file drop', 2)],
+  }),
+  new Category({ name: 'Footer', keywords: [new Keyword('footer', 2)] }),
+  new Category({ name: 'Hero' }),
+  new Category({
+    name: 'Loading/Spinner',
+    keywords: [
+      new Keyword('spinner'),
+      new Keyword('loading'),
+      new Keyword('loader dot'),
+    ],
+  }),
+  new Category({
+    name: 'Modal',
+    keywords: [new Keyword('modal'), new Keyword('popup'), new Keyword('popup box', 2)],
+  }),
+  new Category({
+    name: 'Navigation/Header',
+    keywords: [
+      new Keyword('navigation'),
+      new Keyword('header'),
+      new Keyword('navbar', 2),
+      new Keyword('nav menu'),
+      new Keyword('navabr'),
+      new Keyword('navbars with', 5),
+      new Keyword('responsive navbar', 8),
+      new Keyword('navigation bar', 10),
+      new Keyword('navs'),
+    ],
+  }),
+  new Category({ name: 'Page' }),
+  new Category({
+    name: '404',
+    parent: 'Page',
+    keywords: [new Keyword('404'), new Keyword('not found', 2)],
+  }),
+  new Category({
+    name: 'Pricing',
+    parent: 'Page',
+    keywords: [new Keyword('pricing', 2)],
+  }),
+  new Category({
+    name: 'Testimonial',
+    parent: 'Page',
+    keywords: [new Keyword('testimonial', 2), new Keyword('testimony', 2)],
+  }),
+  new Category({ name: 'Pagination' }),
+  new Category({ name: 'Progress Bar', keywords: [new Keyword('progress')] }),
+  new Category({
+    name: 'Skeleton',
+    keywords: [new Keyword('skeleton')],
+  }),
+  new Category({
+    name: 'Sidebar',
+    keywords: [
+      new Keyword('side panel', 2),
+      new Keyword('side nav', 2),
+      new Keyword('slide-overs', 2),
+      new Keyword('drawer', 2),
+      new Keyword('sliding', 2),
+      new Keyword('Sidenav', 2),
+    ],
+  }),
+  new Category({ name: 'Comment', keywords: [new Keyword('comment form', 2)] }),
+  new Category({ name: 'Step', keywords: [new Keyword('multistep')] }),
+  new Category({
+    name: 'Switch',
+    keywords: [
+      new Keyword('toggle'),
+      new Keyword('toogle'),
+      new Keyword('toggle button', 2),
+    ],
+  }),
+  new Category({
+    name: 'Tags',
+    keywords: [
+      new Keyword('tagline'),
+      new Keyword('pills'),
+      new Keyword('tag line'),
+      new Keyword('chips'),
+      new Keyword('label-tag'),
+    ],
+  }),
+  new Category({
+    name: 'Tooltips',
+    keywords: [new Keyword('tooltip')],
+  }),
+  new Category({ name: 'Table' }),
+  new Category({ name: 'List' }),
+  new Category({ name: 'Tab' }),
+  new Category({ name: 'Timeline' }),
+  new Category({ name: 'Other' }),
 ];
 
 type ComponentItem = {
-    name: string;
-    link: string;
-    category?: string;
-    parent?: string;
+  name: string;
+  link: string;
+  category: string | null;
+  parent: string | null;
 };
 
 export async function updateComponenentsJson({
-    items,
-    componentsJsonPath,
+  items,
+  componentsJsonPath,
 }: GenerateArgs & { componentsJsonPath: string }) {
-    const generated = await generateJson({ items });
-    const components = require(componentsJsonPath) as ComponentItem[];
-    for (const item of generated) {
-        const component = components.find(c => c.link === item.link);
-        if (!component) {
-            components.push(item);
-        } else {
-            component.name = item.name;
-            component.category = item.category;
-            component.parent = item.parent;
-        }
-    }
+  const generated = await generateJson({ items });
+  const components = require(componentsJsonPath) as ComponentItem[];
+  for (const item of generated) {
+    const component = components.find(c => c.link === item.link);
 
-    await fs.writeFile(componentsJsonPath, JSON.stringify(components, null, 2));
+    if (!component) {
+      components.push(item);
+    } else {
+      component.name = item.name;
+      component.category = item.category;
+      component.parent = item.parent;
+    }
+  }
+
+  await fs.writeFile(componentsJsonPath, JSON.stringify(components, null, 2));
 }
 
 async function generateJson({ items }: GenerateArgs) {
-    const categories = groupItems(items);
-    const result: ComponentItem[] = [];
+  const categories = groupItems(items);
+  const result: ComponentItem[] = [];
 
-    for (const category of categoryList) {
-        const items = categories[category.name] || [];
-        if (items.length === 0) {
-            continue;
-        }
-
-        const itemsByLink = _(items)
-            .groupBy(x => x.link)
-            .mapValues(items => {
-                return items.map(x => x.name).join(', ');
-            })
-            .entries()
-            .map(([link, name]) => {
-                return { link, name };
-            })
-            .value();
-
-        for (const item of itemsByLink) {
-            result.push({
-                name: item.name,
-                link: item.link,
-                category: category.name,
-                parent: category.parent,
-            });
-        }
+  for (const category of categoryList) {
+    const items = categories[category.name] || [];
+    if (items.length === 0) {
+      continue;
     }
-    return result;
+
+    const itemsByLink = _(items)
+      .groupBy(x => x.link)
+      .mapValues(items => {
+        return items.map(x => x.name).join(', ');
+      })
+      .entries()
+      .map(([link, name]) => {
+        return { link, name };
+      })
+      .value();
+
+    for (const item of itemsByLink) {
+      result.push({
+        name: item.name,
+        link: item.link,
+        category: category.name || null,
+        parent: category.parent || null,
+      });
+    }
+  }
+  return result;
 }
 
 export async function generateMarkdown({ items }: GenerateArgs) {
-    let content: string[] = ['# Tailwind Components', '## Table of Contents'];
-    const categories = groupItems(items);
+  let content: string[] = ['# Tailwind Components', '## Table of Contents'];
+  const categories = groupItems(items);
 
-    for (const category of categoryList) {
-        const items = categories[category.name] || [];
-        if (items.length === 0) {
-            continue;
-        }
-
-        const itemsByLink = _(items)
-            .groupBy(x => x.link)
-            .mapValues(items => {
-                return items.map(x => x.name).join(', ');
-            })
-            .entries()
-            .map(([link, name]) => {
-                return { link, name };
-            })
-            .value();
-
-        let section = `##${category.parent ? '#' : ''} ${category.name}\n`;
-        section += itemsByLink.map(createLink).join('\n');
-
-        content.push(section);
+  for (const category of categoryList) {
+    const items = categories[category.name] || [];
+    if (items.length === 0) {
+      continue;
     }
-    return content.join('\n');
+
+    const itemsByLink = _(items)
+      .groupBy(x => x.link)
+      .mapValues(items => {
+        return items.map(x => x.name).join(', ');
+      })
+      .entries()
+      .map(([link, name]) => {
+        return { link, name };
+      })
+      .value();
+
+    let section = `##${category.parent ? '#' : ''} ${category.name}\n`;
+    section += itemsByLink.map(createLink).join('\n');
+
+    content.push(section);
+  }
+  return content.join('\n');
 }
 
 /**
  * Group items to categories.
  */
 export function groupItems(items: CompomentLink[]) {
-    const [defaultCategory] = categoryList.slice(-1);
-    const result: { [name: string]: CompomentLink[] } = {};
-    for (const item of items) {
-        const matches = _(categoryList)
-            .map(category => ({
-                category,
-                weight: category.weight(item.name),
-            }))
-            .orderBy(['weight'], ['desc'])
-            .takeWhile(c => c.weight > 0)
-            .thru(collection =>
-                collection.length === 0
-                    ? [{ category: defaultCategory, weight: 0 }]
-                    : collection,
-            )
-            .value();
-        for (const { category } of matches.slice(0, 1)) {
-            result[category.name] = (result[category.name] || []).concat(item);
-        }
+  const [defaultCategory] = categoryList.slice(-1);
+  const result: { [name: string]: CompomentLink[] } = {};
+  for (const item of items) {
+    const matches = _(categoryList)
+      .map(category => ({
+        category,
+        weight: category.weight(item.name),
+      }))
+      .orderBy(['weight'], ['desc'])
+      .takeWhile(c => c.weight > 0)
+      .thru(collection =>
+        collection.length === 0
+          ? [{ category: defaultCategory, weight: 0 }]
+          : collection,
+      )
+      .value();
+    for (const { category } of matches.slice(0, 1)) {
+      result[category.name] = (result[category.name] || []).concat(item);
     }
-    return result;
+  }
+  return result;
 }
 
 export function createLink({ name, link }: CompomentLink) {
-    let description = link.split('//')[1];
-    if (description.startsWith('www.')) {
-        description = description.slice(4);
-    }
-    return `* ${name || '?'}: [${description}](${link})`;
+  let description = link.split('//')[1];
+  if (description.startsWith('www.')) {
+    description = description.slice(4);
+  }
+  return `* ${name || '?'}: [${description}](${link})`;
 }
--- src/program.ts
+++ src/program.ts
@@ -13,56 +13,56 @@ import { CompomentLink } from './types';
  */
 
 if (require.main?.filename === __filename) {
-    program(yargs.argv as ProgramOptions);
+  program(yargs.argv as ProgramOptions);
 }
 
 type ProgramOptions = {
-    only?: string;
-    slowmo?: number;
-    format?: string;
+  only?: string;
+  slowmo?: number;
+  format?: string;
 };
 
 async function program(options?: ProgramOptions) {
-    const browser = await puppeteer.launch({
-        headless: true,
-        slowMo: options?.slowmo,
-        defaultViewport: null,
-        args: ['--start-maximized'],
-    });
-    const [page] = await browser.pages();
-    const scrapers = await getScrapers({ name: options?.only });
-    const items: CompomentLink[] = [];
-    for await (const [index, scraper] of scrapers.entries()) {
-        let tryCount = 3;
-        while (tryCount--) {
-            process.stdout.write(
-                `\nProgress: ${scraper.name} ${index + 1}/${scrapers.length} ...`,
-            );
-            try {
-                let scraperItems = await scraper({ page });
-                process.stdout.write(` +${scraperItems.length}`);
-                items.push(...scraperItems);
-                break;
-            } catch (error) {
-                console.error(error);
-                console.log(`Tries left: ${tryCount}`);
-            }
-        }
-        process.stdout.write(` Total: ${items.length}`);
+  const browser = await puppeteer.launch({
+    headless: true,
+    slowMo: options?.slowmo,
+    defaultViewport: null,
+    args: ['--start-maximized'],
+  });
+  const [page] = await browser.pages();
+  const scrapers = await getScrapers({ name: options?.only });
+  const items: CompomentLink[] = [];
+  for await (const [index, scraper] of scrapers.entries()) {
+    let tryCount = 3;
+    while (tryCount--) {
+      process.stdout.write(
+        `\nProgress: ${scraper.name} ${index + 1}/${scrapers.length} ...`,
+      );
+      try {
+        let scraperItems = await scraper({ page });
+        process.stdout.write(` +${scraperItems.length}`);
+        items.push(...scraperItems);
+        break;
+      } catch (error) {
+        console.error(error);
+        console.log(`Tries left: ${tryCount}`);
+      }
     }
-    await browser.close();
-    switch (options?.format) {
-        case 'markdown':
-            {
-                await generateMarkdown({ items });
-                const content = await generateMarkdown({ items });
-                await fs.writeFile('README.md', content);
-            }
-            break;
-        default:
-            await updateComponenentsJson({
-                items,
-                componentsJsonPath: require.resolve(process.cwd() + '/components.json'),
-            });
-    }
+    process.stdout.write(` Total: ${items.length}`);
+  }
+  await browser.close();
+  switch (options?.format) {
+    case 'markdown':
+      {
+        await generateMarkdown({ items });
+        const content = await generateMarkdown({ items });
+        await fs.writeFile('README.md', content);
+      }
+      break;
+    default:
+      await updateComponenentsJson({
+        items,
+        componentsJsonPath: require.resolve(process.cwd() + '/components.json'),
+      });
+  }
 }
