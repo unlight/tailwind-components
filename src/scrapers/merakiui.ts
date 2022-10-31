a735ea1f8ce1c8a8598adb4b8c6b1e9e4b922bad
Roman_Vasilev <s91630277@gmail.com>
2022-10-31 19:35:13 +0400

    fix: merakiui

--- src/scrapers/merakiui.ts
+++ src/scrapers/merakiui.ts
@@ -1,23 +1,37 @@
 import { CompomentLink, ScraperArgs } from '../types';
+import _ from 'lodash';
 
 export default async function merakiui({
-    page,
+  page,
 }: ScraperArgs): Promise<CompomentLink[]> {
-    await page.goto('https://merakiui.com/components', { waitUntil: 'networkidle0' });
-    const sections = await page.$$('nav button');
-    const result: CompomentLink[] = [];
-    for (const section of sections) {
-        await section.click();
-        const category = await page.$eval('h1.capitalize', h => h.textContent);
-        const names = await page.$$eval('h3.capitalize', elements =>
-            elements.map(h => h.textContent),
-        );
-        names.forEach(name => {
-            result.push({
-                name: `${category} ${name}`,
-                link: 'https://merakiui.com/',
-            });
-        });
+  const result: CompomentLink[] = [];
+  await page.goto('https://merakiui.com/components', {
+    waitUntil: 'networkidle0',
+  });
+  const sections = await page.$$eval('a[href^="/components/"]', elements =>
+    elements.map(a => {
+      const category = a.querySelector('h2')?.textContent?.trim();
+      const link = (a as HTMLAnchorElement).href;
+
+      return { category, link };
+    }),
+  );
+
+  for (const section of sections) {
+    await page.goto(section.link);
+    let names = (await page.$$eval('main h2', elements => {
+      return elements.map(e => e.textContent?.trim());
+    })) as string[];
+    names = _(names).filter(Boolean).uniq().value();
+
+    for (const name of names) {
+      result.push({
+        category: section.category,
+        name: `${section.category} ${name}`,
+        link: section.link,
+      });
     }
-    return result;
+  }
+
+  return result;
 }
