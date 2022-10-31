72d5ece89bb998b49d665517fe01fcb6e3427ad3
Roman_Vasilev <s91630277@gmail.com>
2022-10-31 19:53:33 +0400

    fix: flowbite

--- src/scrapers/flowbite.ts
+++ src/scrapers/flowbite.ts
@@ -1,38 +1,34 @@
 import { CompomentLink, ScraperArgs } from '../types';
 
 export default async function flowbite({
-    page,
+  page,
 }: ScraperArgs): Promise<CompomentLink[]> {
-    const result: CompomentLink[] = [];
-    await page.goto('https://flowbite.com/docs/components/alerts/', {
-        waitUntil: 'networkidle0',
+  const result: CompomentLink[] = [];
+  await page.goto('https://flowbite.com/docs/components/alerts/', {
+    waitUntil: 'networkidle0',
+  });
+  const links = await page.$$eval('a[href^="/docs/components/"]', elements => {
+    return elements.map(a => {
+      const category = a.textContent?.replace(' NEW', '').trim();
+      return {
+        category,
+        link: (a as HTMLAnchorElement).href,
+      };
     });
-    const links = await page.$$eval('a[href^="/docs/components/"]', elements =>
-        elements.map(a => {
-            return {
-                category: a.textContent!.trim(),
-                link: (a as HTMLAnchorElement).href,
-            };
-        }),
-    );
-    for (const { category, link } of links) {
-        await page.goto(link, { waitUntil: 'networkidle0' });
-        const elements = await page.$x(
-            '//h5[contains(text(), "On this page")]/../..//a',
-        );
-        for (const element of elements) {
-            const anchorName = await element.evaluate(x => {
-                return x.textContent!.trim();
-            });
-            if (anchorName === 'JavaScript') {
-                continue;
-            }
-            result.push({
-                name: `${category} ${anchorName}`,
-                link,
-            });
-        }
-    }
+  });
 
-    return result;
+  for (const { category, link } of links.slice(0, 2)) {
+    await page.goto(link, { waitUntil: 'networkidle0' });
+    const names = (
+      await page.$$eval('main h2', elements => {
+        return elements.map(e => e.childNodes[0].textContent?.trim());
+      })
+    ).filter(Boolean) as string[];
+
+    for (const name of names) {
+      result.push({ category, name: `${category} ${name}`, link });
+    }
+  }
+
+  return result;
 }
