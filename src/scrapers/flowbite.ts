dc14f4641676ec3cad2892ee69873fc4c9899188
Roman_Vasilev <s91630277@gmail.com>
2022-10-31 20:23:25 +0400

    fix: flowbite

--- src/scrapers/flowbite.ts
+++ src/scrapers/flowbite.ts
@@ -7,17 +7,25 @@ export default async function flowbite({
   await page.goto('https://flowbite.com/docs/components/alerts/', {
     waitUntil: 'networkidle0',
   });
-  const links = await page.$$eval('a[href^="/docs/components/"]', elements => {
+  const links = await page.$$eval('a[href^="/docs/"]', elements => {
     return elements.map(a => {
-      const category = a.textContent?.replace(' NEW', '').trim();
-      return {
-        category,
-        link: (a as HTMLAnchorElement).href,
-      };
+      const link = (a as HTMLAnchorElement).href;
+      if (
+        link.includes('/components/') ||
+        link.includes('/forms/') ||
+        link.includes('/typography/') ||
+        link.includes('/plugins/')
+      ) {
+        const category = a.textContent?.replace(' NEW', '').trim();
+        return {
+          category,
+          link: (a as HTMLAnchorElement).href,
+        };
+      }
     });
   });
 
-  for (const { category, link } of links.slice(0, 2)) {
+  for (const { category, link } of links.filter(Boolean) as CompomentLink[]) {
     await page.goto(link, { waitUntil: 'networkidle0' });
     const names = (
       await page.$$eval('main h2', elements => {
