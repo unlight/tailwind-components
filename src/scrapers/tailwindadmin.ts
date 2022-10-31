0e6d74faa44c942ff27ade7577752462b28a3c0b
Roman_Vasilev <s91630277@gmail.com>
2022-10-31 19:40:25 +0400

    fix: tailwindadmin

--- src/scrapers/tailwindadmin.ts
+++ src/scrapers/tailwindadmin.ts
@@ -1,39 +1,39 @@
 import { CompomentLink, ScraperArgs } from '../types';
 
 export default async function tailwindadmin({
-    page,
+  page,
 }: ScraperArgs): Promise<CompomentLink[]> {
-    const result: CompomentLink[] = [];
-    await page.goto('https://tailwindadmin.netlify.app');
-    const links = await page.$$eval(
-        'aside[class*="md:block"] li a:not(.mx-4)',
-        elements =>
-            elements.slice(1).map(element => ({
-                section: element.textContent!.trim(),
-                href: (element as HTMLAnchorElement).href,
-            })),
+  const result: CompomentLink[] = [];
+  await page.goto('https://tailwindadmin.netlify.app');
+  const links = await page.$$eval(
+    'aside[class*="md:block"] li a:not(.mx-4)',
+    elements =>
+      elements.slice(1).map(element => ({
+        section: element.textContent!.trim(),
+        href: (element as HTMLAnchorElement).href,
+      })),
+  );
+  for (const { href, section } of links) {
+    await page.goto(href, { waitUntil: 'networkidle0' });
+    const names = await page.$$eval(
+      '.border-solid.rounded.border.w-full > div:first-child',
+      elements => elements.map(x => x.textContent!.trim()),
     );
-    for (const { href, section } of links) {
-        await page.goto(href);
-        const names = await page.$$eval(
-            '.border-solid.rounded.border.w-full > div:first-child',
-            elements => elements.map(x => x.textContent!.trim()),
-        );
-        names.forEach(name => {
-            result.push({ name: `${section} ${name}`, link: href });
-        });
-    }
-    // Pages
-    const pageLinks = await page.$$eval(
-        'aside[class*="md:block"] li a.mx-4',
-        elements =>
-            elements.slice(1).map(element => ({
-                name: element.textContent!.trim(),
-                link: (element as HTMLAnchorElement).href,
-            })),
-    );
+    names.forEach(name => {
+      result.push({ name: `${section} ${name}`, link: href });
+    });
+  }
+  // Pages
+  const pageLinks = await page.$$eval(
+    'aside[class*="md:block"] li a.mx-4',
+    elements =>
+      elements.slice(1).map(element => ({
+        name: element.textContent!.trim(),
+        link: (element as HTMLAnchorElement).href,
+      })),
+  );
 
-    result.push(...pageLinks);
+  result.push(...pageLinks);
 
-    return result;
+  return result;
 }
