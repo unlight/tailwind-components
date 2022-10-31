e9e2e69fcbdac2fd97ed548d5853d7baca44c99a
Roman_Vasilev <s91630277@gmail.com>
2022-10-31 20:57:10 +0400

    format: tailwindelements

--- src/scrapers/tailwindelements.ts
+++ src/scrapers/tailwindelements.ts
@@ -2,34 +2,34 @@ import { uniq } from 'lodash';
 import { CompomentLink, ScraperArgs } from '../types';
 
 export default async function tailwindelements({
-    page,
+  page,
 }: ScraperArgs): Promise<CompomentLink[]> {
-    const result: CompomentLink[] = [];
-    await page.goto('https://tailwind-elements.com/quick-start/', {
-        waitUntil: 'networkidle0',
+  const result: CompomentLink[] = [];
+  await page.goto('https://tailwind-elements.com/quick-start/', {
+    waitUntil: 'networkidle0',
+  });
+  const links = await page.$$eval(
+    'a[href^="/docs/standard/components/"]',
+    elements => {
+      return elements.map(a => ({
+        link: (a as HTMLAnchorElement).href,
+        category: a.textContent!.trim(),
+      }));
+    },
+  );
+  for (const { link, category } of links) {
+    await page.goto(link, { waitUntil: 'networkidle0' });
+    let names = await page.$$eval('main h2', elements => {
+      return elements.map(element => element.textContent!.trim());
     });
-    const links = await page.$$eval(
-        'a[href^="/docs/standard/components/"]',
-        elements => {
-            return elements.map(a => ({
-                link: (a as HTMLAnchorElement).href,
-                category: a.textContent!.trim(),
-            }));
-        },
-    );
-    for (const { link, category } of links) {
-        await page.goto(link, { waitUntil: 'networkidle0' });
-        let names = await page.$$eval('main h2', elements => {
-            return elements.map(element => element.textContent!.trim());
-        });
-        names = uniq(names);
-        for (let name of names) {
-            if (!name.startsWith(category)) {
-                name = `${category} ${name}`;
-            }
-            result.push({ link, name });
-        }
+    names = uniq(names);
+    for (let name of names) {
+      if (!name.startsWith(category)) {
+        name = `${category} ${name}`;
+      }
+      result.push({ link, name });
     }
+  }
 
-    return result;
+  return result;
 }
