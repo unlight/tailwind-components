74fd9892322e33c030b0f9a7493f0579e77e613e
Roman_Vasilev <s91630277@gmail.com>
2023-07-15 18:40:18 +0400

    fix: Disable gustui

--- src/scrapers/gustui.ts
+++ src/scrapers/gustui.ts
@@ -1,44 +1,44 @@
 import { CompomentLink, ScraperArgs } from '../types';
 
-export default async function gustui({ page }: ScraperArgs): Promise<CompomentLink[]> {
-  const result: CompomentLink[] = [];
-  await page.goto('https://www.gustui.com/docs', {
-    waitUntil: 'load',
-  });
+// export default async function gustui({ page }: ScraperArgs): Promise<CompomentLink[]> {
+//   const result: CompomentLink[] = [];
+//   await page.goto('https://www.gustui.com/docs', {
+//     waitUntil: 'load',
+//   });
 
-  page.waitForNavigation({ timeout: 60_000 });
+//   page.waitForNavigation({ timeout: 60_000 });
 
-  const sectionLinks = await page.$$eval('a[href^="/docs/application/"]', elements =>
-    elements.map(a => a.getAttribute('href')),
-  );
-  for (const sectionLink of sectionLinks) {
-    await page.goto(`https://www.gustui.com${sectionLink}`, {});
-    const section = await page.$(`a[href="${sectionLink}"]`);
-    const hrefs = (await section!
-      .evaluateHandle(c => {
-        return Array.from(
-          c.nextElementSibling!.querySelectorAll('a[href^="/docs/application/"]'),
-        ).map((element: any) => element.getAttribute('href'));
-      })
-      .then(hrefs => hrefs.jsonValue())) as string[];
+//   const sectionLinks = await page.$$eval('a[href^="/docs/application/"]', elements =>
+//     elements.map(a => a.getAttribute('href')),
+//   );
+//   for (const sectionLink of sectionLinks) {
+//     await page.goto(`https://www.gustui.com${sectionLink}`, {});
+//     const section = await page.$(`a[href="${sectionLink}"]`);
+//     const hrefs = (await section!
+//       .evaluateHandle(c => {
+//         return Array.from(
+//           c.nextElementSibling!.querySelectorAll('a[href^="/docs/application/"]'),
+//         ).map((element: any) => element.getAttribute('href'));
+//       })
+//       .then(hrefs => hrefs.jsonValue())) as string[];
 
-    for (const href of hrefs) {
-      const link = `https://www.gustui.com${href}`;
-      await page.goto(link, { waitUntil: 'networkidle0' });
-      const category = await page.$eval('h1', h => h.textContent);
-      const section = await page
-        .$('h1')
-        .then(x => x!.evaluateHandle(h => h.nextElementSibling));
-      const names = (await section.evaluate((s: any) =>
-        Array.from<Element>(s.querySelectorAll('[id] span.text-lg')).map(
-          x => x.textContent!,
-        ),
-      )) as string[];
-      for (const name of names) {
-        result.push({ name: `${category} ${name}`, link });
-      }
-    }
-  }
+//     for (const href of hrefs) {
+//       const link = `https://www.gustui.com${href}`;
+//       await page.goto(link, { waitUntil: 'networkidle0' });
+//       const category = await page.$eval('h1', h => h.textContent);
+//       const section = await page
+//         .$('h1')
+//         .then(x => x!.evaluateHandle(h => h.nextElementSibling));
+//       const names = (await section.evaluate((s: any) =>
+//         Array.from<Element>(s.querySelectorAll('[id] span.text-lg')).map(
+//           x => x.textContent!,
+//         ),
+//       )) as string[];
+//       for (const name of names) {
+//         result.push({ name: `${category} ${name}`, link });
+//       }
+//     }
+//   }
 
-  return result;
-}
+//   return result;
+// }
