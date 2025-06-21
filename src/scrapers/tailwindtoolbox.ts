64493e64ea5a23dfcd80d438fbc5ebc634d994c0
roman <>
2025-06-21 22:24:11 +0400

    fix: Tailwindtoolbox

--- src/scrapers/tailwindtoolbox.ts
+++ src/scrapers/tailwindtoolbox.ts
@@ -2,22 +2,25 @@ import { CompomentLink, ScraperArgs } from '../types';
 import { Page } from 'puppeteer';
 
 export default async function tailwindtoolbox({
-    page,
+  page,
 }: ScraperArgs): Promise<CompomentLink[]> {
-    const result: CompomentLink[] = [];
-    await page.goto('https://www.tailwindtoolbox.com/starter-components');
-    result.push(...(await getCards(page)));
-    await page.goto('https://www.tailwindtoolbox.com/starter-templates');
-    result.push(...(await getCards(page)));
-    return result;
+  const result: CompomentLink[] = [];
+  await page.goto('https://www.tailwindtoolbox.com/starter-components');
+  result.push(...(await getCards(page)));
+
+  await page.goto('https://www.tailwindtoolbox.com/starter-templates');
+  result.push(...(await getCards(page)));
+
+  return result;
 }
 
 async function getCards(page: Page) {
-    return page.$$eval('.card', elements =>
-        elements.map(card => {
-            const link = (card.querySelector('a[href]') as HTMLAnchorElement).href;
-            const name = card.querySelector('.card-title')!.textContent!.trim();
-            return { name, link };
-        }),
-    );
+  return page.$$eval('[data-twcat]', elements =>
+    elements.map(card => {
+      const link = (card.querySelector('a[href]') as HTMLAnchorElement).href;
+      const name = card.querySelector('.card-title')!.textContent!.trim();
+
+      return { name, link };
+    }),
+  );
 }
